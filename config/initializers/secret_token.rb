953615234561944681ae9756c1c8bc2a05f7ec36
Payton Swick <payton@foolord.com>
2013-01-30 14:54:28 -0500

    create initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Littleboxes::Application.config.secret_token = '41e3fc1a9aa21b5d609ec76338a7f1de39d80633d9da76ce8606ae18a673f9cee1e95e6292d9dc780b591610ec9387b3bb1cc386848a909e8c28fc6b82d7a994'
